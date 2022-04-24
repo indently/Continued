bbb81f20e76890e7bb2be83d1ff6331083fe0e4a
Federico <codepalace@fastmail.se>
2022-04-24 10:10:47 +0200

    Changed the text.

--- Continued/ContentView.swift
+++ Continued/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("I hope no one will read this!")
             .padding()
     }
 }
