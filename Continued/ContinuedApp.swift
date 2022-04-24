7365e75e7b7c8ece59cefd378cf342d60aafed87
Federico <codepalace@fastmail.se>
2022-04-24 10:10:05 +0200

    Initial Commit

---
+++ Continued/ContinuedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedApp.swift
+//  Continued
+//
+//  Created by Federico on 24/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
